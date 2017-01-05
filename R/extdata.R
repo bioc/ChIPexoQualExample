56f19747ecb6ca8fc1fbc6b10ba495790881e4ed
Martin Morgan <mtmorgan@fhcrc.org>
2017-01-05 23:11:26 +0000

    Adding ChIPexoQualExample
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/ChIPexoQualExample@4043 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/extdata.R
@@ -0,0 +1,16 @@
+##' FoxA1 ChIP-exo experiment
+##' 
+##' bam file with the reads aligned to chr1 from FoxA1 in mm9 liver cell lines
+##' published by Serandour et al., 2013. This package contains the files
+##' 
+##' \itemize{
+##' \item ChIPexo_carroll_FoxA1_mouse_rep1_chr1.bam
+##' \item ChIPexo_carroll_FoxA1_mouse_rep2_chr1.bam
+##' \item ChIPexo_carroll_FoxA1_mouse_rep3_chr1.bam
+##' }
+##' and their respective index files.
+##' 
+##' @docType data
+##' @name extdata
+##' @format bam files (with their respective indexes)
+NULL
